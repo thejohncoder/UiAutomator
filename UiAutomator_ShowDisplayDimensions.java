747f8a49e1dc192c6e70cc6631501c44d59ba363
thejohncoder <thejohn848@gmail.com>
2016-11-28 17:05:22 -0500

    Update UiAutomator_ShowDisplayDimensions.java

--- UiAutomator_ShowDisplayDimensions.java
+++ UiAutomator_ShowDisplayDimensions.java
@@ -1,5 +1,8 @@
 //// Utility function to get the Display Dimensions information of currently running device.
 
+//// The info is output to AndroidStudio's Log window
+
+
 public void funcShowDisplayDimensions() {
 
   Log.i("Display Dimensions", "Wd: " + mDevice.getDisplayWidth() + " | Ht: " + mDevice.getDisplayHeight());
