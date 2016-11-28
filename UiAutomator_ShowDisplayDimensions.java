eb3b6d09773fe1b17fcd3839ce8b46ec0a63c0b0
thejohncoder <thejohn848@gmail.com>
2016-11-28 13:08:51 -0500

    Create UiAutomator_ShowDisplayDimensions.java

---
+++ UiAutomator_ShowDisplayDimensions.java
@@ -0,0 +1,7 @@
+//// Utility function to get the Display Dimensions information of currently running device.
+
+public void funcShowDisplayDimensions() {
+
+  Log.i("Display Dimensions", "Wd: " + mDevice.getDisplayWidth() + " | Ht: " + mDevice.getDisplayHeight());
+
+}
